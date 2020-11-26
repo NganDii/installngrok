0351694e63b96777ddd99b75b28300e442ad300b
RemApple <32689703+NganDii@users.noreply.github.com>
2020-11-26 19:34:11 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,6 +3,9 @@ unzip $loc/ngrok-stable-linux-arm.zip
 mv $loc/ngrok ~/
 chmod +x ~/ngrok
 cd $HOME
+echo "\nEnter your Authtoken: "
+read r
+./ngrok authtoken $r
 echo -e "\nTesting ngrok server in...\n"
 echo -ne "\r3"
 sleep 1
@@ -12,4 +15,5 @@ echo -ne "\r1"
 sleep 1
 ./ngrok http 8080
 rm -rf $loc/ngrok-stable-linux-arm.zip
+clear
 echo "Ngrok Installation Successful"
